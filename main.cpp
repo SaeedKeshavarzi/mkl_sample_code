724d72caaeef5abd616f3041c3094c25061f7b72
Saeed Keshavarzi <keshavarzi1991@gmail.com>
2018-04-27 03:01:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,79 +1,55 @@
 #include <stdio.h>
+#include <chrono>
+
 #include <mkl.h>
 
-#define min(x,y) (((x) < (y)) ? (x) : (y))
+#define MAT_SIZE 300
+#define MAT_EL_COUNT (MAT_SIZE * MAT_SIZE)
+#define N_ITERATE 10000
 
 int main()
 {
 	int ret = 0;
 
 	double *A, *B, *C;
-	int m, n, k, i, j;
-	double alpha, beta;
+	double alpha{ 1. }, beta{ 0. };
 
-	printf("\n This example computes real matrix C=alpha*A*B+beta*C using \n"
-		" Intel(R) MKL function dgemm, where A, B, and  C are matrices and \n"
-		" alpha and beta are double precision scalars\n\n");
-
-	m = 2000, k = 200, n = 1000;
-	printf(" Initializing data for matrix multiplication C=A*B for matrix \n"
-		" A(%ix%i) and matrix B(%ix%i)\n\n", m, k, k, n);
-	alpha = 1.0; beta = 0.0;
-
-	printf(" Allocating memory for matrices aligned on 64-byte boundary for better \n"
-		" performance \n\n");
-	A = (double *)mkl_malloc(m*k * sizeof(double), 64);
-	B = (double *)mkl_malloc(k*n * sizeof(double), 64);
-	C = (double *)mkl_malloc(m*n * sizeof(double), 64);
-	if (A == NULL || B == NULL || C == NULL) {
+	A = (double *)mkl_malloc(MAT_EL_COUNT * sizeof(double), 64);
+	B = (double *)mkl_malloc(MAT_EL_COUNT * sizeof(double), 64);
+	C = (double *)mkl_malloc(MAT_EL_COUNT * sizeof(double), 64);
+	if (A == NULL || B == NULL || C == NULL)
+	{
 		printf("\n ERROR: Can't allocate memory for matrices. Aborting... \n\n");
+
 		mkl_free(A);
 		mkl_free(B);
 		mkl_free(C);
+
 		ret = 1;
 		goto SAY_GOODBYE;
 	}
 
-	printf(" Intializing matrix data \n\n");
-	for (i = 0; i < (m*k); i++) {
-		A[i] = (double)(i + 1);
-	}
-
-	for (i = 0; i < (k*n); i++) {
-		B[i] = (double)(-i - 1);
-	}
-
-	for (i = 0; i < (m*n); i++) {
-		C[i] = 0.0;
-	}
-
-	printf(" Computing matrix product using Intel(R) MKL dgemm function via CBLAS interface \n\n");
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-		m, n, k, alpha, A, k, B, n, beta, C, n);
-	printf("\n Computations completed.\n\n");
-
-	printf(" Top left corner of matrix A: \n");
-	for (i = 0; i<min(m, 6); i++) {
-		for (j = 0; j<min(k, 6); j++) {
-			printf("%12.0f", A[j + i*k]);
+	srand(std::chrono::steady_clock::now().time_since_epoch().count());
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < MAT_EL_COUNT; ++j)
+		{
+			A[j] = (rand() % MAT_SIZE) / (MAT_SIZE * 0.5);
+			B[j] = (rand() % MAT_SIZE) / (MAT_SIZE * 0.5);
 		}
-		printf("\n");
-	}
 
-	printf("\n Top left corner of matrix B: \n");
-	for (i = 0; i<min(k, 6); i++) {
-		for (j = 0; j<min(n, 6); j++) {
-			printf("%12.0f", B[j + i*n]);
-		}
-		printf("\n");
-	}
+		auto _1 = std::chrono::steady_clock::now();
 
-	printf("\n Top left corner of matrix C: \n");
-	for (i = 0; i<min(m, 6); i++) {
-		for (j = 0; j<min(n, 6); j++) {
-			printf("%12.5G", C[j + i*n]);
+		for (int k = 0; k < N_ITERATE; ++k)
+		{
+			cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+				MAT_SIZE, MAT_SIZE, MAT_SIZE, alpha, A, MAT_SIZE, B, MAT_SIZE, beta, C, MAT_SIZE);
 		}
-		printf("\n");
+
+		auto _2 = std::chrono::steady_clock::now();
+		auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(_2 - _1).count();
+
+		printf("%lld ms, %lld multiply/sec \n", diff, (N_ITERATE * 1000) / diff);
 	}
 
 	printf("\n Deallocating memory \n\n");
